65b4ec50a8c4a568e3a6ca723f8d9b81761ad495
TzipiDunner <tzipi0548529@gmail.com>
2023-06-25 00:56:17 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,63 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ToDoDbContext>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("myPolicy",
+                        policy =>
+                        {
+                            policy.AllowAnyOrigin()
+                                  .AllowAnyHeader()
+                                  .AllowAnyMethod();
+                        });
+});
+var app = builder.Build();
+
+app.UseCors("myPolicy");
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+});
+
+app.MapGet("/todoitems", (ToDoDbContext context) =>
+{
+    return context.Items.ToList();
+});
+
+app.MapPost("/todoitems", async (ToDoDbContext context, Item item) =>
+{
+    if (context.Items.Count() > 0)
+        item.Id = await context.Items.MaxAsync(i => i.Id) + 1;
+    else
+        item.Id = 1;
+    context.Items.Add(item);
+    await context.SaveChangesAsync();
+    return item;
+});
+
+app.MapPut("/todoitems/{id}", async (ToDoDbContext context, Item item, int id) =>
+{
+    var todoItem = await context.Items.FindAsync(id);
+    if (todoItem is null) return Results.NotFound();
+    todoItem.IsComplete = item.IsComplete;
+    await context.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.MapDelete("/todoitems/{id}", async (ToDoDbContext context, int id) =>
+{
+    if (await context.Items.FindAsync(id) is Item item)
+    {
+        context.Items.Remove(item);
+        await context.SaveChangesAsync();
+        return Results.Ok(item);
+    }
+    return Results.NotFound();
+});
+
+app.Run();
